23cab382a96b4353ba12581e1d24c58937c58a8b
David Pan <david.gl.pan@gmail.com>
2017-07-21 15:21:16 +0800

    add login and logout

--- routes/api/authentication.js
+++ routes/api/authentication.js
@@ -4,7 +4,7 @@ const User = require('../../models/user.js');
 
 const router = express.Router();
 
-//POST to /register
+// POST to /register
 router.post('/register', (req, res) => {
   // Create a user object to save, using values from incoming JSON
   const newUser = new User({
@@ -23,6 +23,25 @@ router.post('/register', (req, res) => {
     // Otherwise, for now, send back a JSON object with the new user's info
     return res.send(JSON.stringify(user));
   });
+});
+
+// POST to /login
+router.post('/login', (req, res) => {
+  passport.authenticate('local')(req, res, () => {
+    // If logged in, we should have user info to send back
+    if (req.user) {
+      return res.send(JSON.stringify(req.user));
+    }
+
+    // Otherwise return an error
+    return res.send(JSON.stringify({ error: 'There was an error logging in' }));
+  });
+});
+
+// GET to /logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  return res.send(JSON.stringify(req.user));
 });
 
 module.exports = router;
